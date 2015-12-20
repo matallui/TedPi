6cf5d965f6d4fff935e85fe9ae34d1f45204a7bb
matallui <matallui@gmail.com>
2015-12-20 16:35:59 +0000

    remove newlib support

--- cmd.c
+++ cmd.c
@@ -1,6 +1,3 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
 #include "clib.h"
 #include "twi.h"
 #include "cmd.h"
--- irq.c
+++ irq.c
@@ -1,4 +1,3 @@
-//#include <stdio.h>
 #include "clib.h"
 #include "kernel.h"
 #include "systimer.h"
